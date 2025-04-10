fc21133de967f8275975233735dd7428b7b3b80f
Chronicle20 <a.chronicle.20@gmail.com>
2025-04-10 07:49:34 -0400

    Add resources for creating and deleting character inventory.

--- atlas.com/inventory/inventory/resource.go
+++ atlas.com/inventory/inventory/resource.go
@@ -19,6 +19,8 @@ func InitResource(si jsonapi.ServerInformation) func(db *gorm.DB) server.RouteIn
 			registerGet := rest.RegisterHandler(l)(si)
 			r := router.PathPrefix("/characters/{characterId}/inventory").Subrouter()
 			r.HandleFunc("", registerGet("get_inventory", handleGetInventory(db))).Methods(http.MethodGet)
+			r.HandleFunc("", registerGet("create_default_inventory", handleCreateInventory(db))).Methods(http.MethodPost)
+			r.HandleFunc("", registerGet("delete_inventory", handleDeleteInventory(db))).Methods(http.MethodDelete)
 		}
 	}
 }
@@ -37,7 +39,7 @@ func handleGetInventory(db *gorm.DB) rest.GetHandler {
 					return
 				}
 
-				res, err := model.Map(Transform)(model.FixedProvider(m))()
+				rm, err := model.Map(Transform)(model.FixedProvider(m))()
 				if err != nil {
 					d.Logger().WithError(err).Errorf("Creating REST model.")
 					w.WriteHeader(http.StatusInternalServerError)
@@ -46,7 +48,48 @@ func handleGetInventory(db *gorm.DB) rest.GetHandler {
 
 				query := r.URL.Query()
 				queryParams := jsonapi.ParseQueryFields(&query)
-				server.MarshalResponse[RestModel](d.Logger())(w)(c.ServerInformation())(queryParams)(res)
+				server.MarshalResponse[RestModel](d.Logger())(w)(c.ServerInformation())(queryParams)(rm)
+			}
+		})
+	}
+}
+
+func handleCreateInventory(db *gorm.DB) rest.GetHandler {
+	return func(d *rest.HandlerDependency, c *rest.HandlerContext) http.HandlerFunc {
+		return rest.ParseCharacterId(d.Logger(), func(characterId uint32) http.HandlerFunc {
+			return func(w http.ResponseWriter, r *http.Request) {
+				m, err := NewProcessor(d.Logger(), d.Context(), db).CreateAndEmit(characterId)
+				if err != nil {
+					d.Logger().WithError(err).Errorf("Unable to create inventory for character [%d].", characterId)
+					w.WriteHeader(http.StatusInternalServerError)
+					return
+				}
+
+				rm, err := model.Map(Transform)(model.FixedProvider(m))()
+				if err != nil {
+					d.Logger().WithError(err).Errorf("Creating REST model.")
+					w.WriteHeader(http.StatusInternalServerError)
+					return
+				}
+
+				query := r.URL.Query()
+				queryParams := jsonapi.ParseQueryFields(&query)
+				server.MarshalResponse[RestModel](d.Logger())(w)(c.ServerInformation())(queryParams)(rm)
+			}
+		})
+	}
+}
+
+func handleDeleteInventory(db *gorm.DB) rest.GetHandler {
+	return func(d *rest.HandlerDependency, c *rest.HandlerContext) http.HandlerFunc {
+		return rest.ParseCharacterId(d.Logger(), func(characterId uint32) http.HandlerFunc {
+			return func(w http.ResponseWriter, r *http.Request) {
+				err := NewProcessor(d.Logger(), d.Context(), db).DeleteAndEmit(characterId)
+				if err != nil {
+					d.Logger().WithError(err).Errorf("Unable to create inventory for character [%d].", characterId)
+					w.WriteHeader(http.StatusInternalServerError)
+					return
+				}
 			}
 		})
 	}
